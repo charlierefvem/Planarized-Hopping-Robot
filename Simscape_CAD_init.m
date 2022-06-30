30e96ab3e31e79607c774879b8f536b1f3074233
jack33001 <51167808+jack33001@users.noreply.github.com>
2022-06-30 08:58:25 -0700

    Update Simscape_CAD_init.m
    Made it path dependent instead of handle dependent

--- Simscape_CAD_init.m
+++ Simscape_CAD_init.m
@@ -1,17 +1,17 @@
 filepath = strcat(pwd,"/Simscape_CAD/");
 
-shank_handle = 2.010001220703125e+02;
-thigh_handle = 2.130001220703125e+02;
-pulley_handle = 2.960001220703125e+02;
-knee_motor_handle = 2.630001220703125e+02;
-knee_plate_handle = 2.650001220703125e+02;
-hip_motor_handle = 1.350001220703125e+02;
-hip_plate_handle = 1.370001220703125e+02;
+shank_path = "Simscape_CAD/Hopping Robot Model/Hopping Robot Mechanics/Shank Link";
+thigh_path = "Simscape_CAD/Hopping Robot Model/Hopping Robot Mechanics/Thigh Link";
+pulley_path = "Simscape_CAD/Hopping Robot Model/Pulley";
+knee_motor_path = "Simscape_CAD/Hopping Robot Model/Knee Motor AK80-6";
+knee_plate_path = "Simscape_CAD/Hopping Robot Model/Knee Motor Plate"
+hip_motor_path = "Simscape_CAD/Hopping Robot Model/Hip Motor AK80-6";
+hip_plate_path = "Simscape_CAD/Hopping Robot Model/Hip Plate";
 
-set_param(shank_handle,"ExtGeomFileName",strcat(filepath,"shank.STEP"));
-set_param(thigh_handle,"ExtGeomFileName",strcat(filepath,"thigh.STEP"))
-set_param(pulley_handle,"ExtGeomFileName",strcat(filepath,"pulley.STEP"))
-set_param(knee_motor_handle,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"))
-set_param(knee_plate_handle,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"))
-set_param(hip_motor_handle,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"))
-set_param(hip_plate_handle,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"))
+set_param(shank_path,"ExtGeomFileName",strcat(filepath,"shank.STEP"));
+set_param(thigh_path,"ExtGeomFileName",strcat(filepath,"thigh.STEP"))
+set_param(pulley_path,"ExtGeomFileName",strcat(filepath,"pulley.STEP"))
+set_param(knee_motor_path,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"))
+set_param(knee_plate_path,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"))
+set_param(hip_motor_path,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"))
+set_param(hip_plate_path,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"))
