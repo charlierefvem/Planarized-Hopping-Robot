7daa4dbac7d22033b4ccf41e2b96fb559b067f8b
jack33001 <51167808+jack33001@users.noreply.github.com>
2022-08-18 10:59:51 -0700

    Cleanup
    Standardized the formatting of all the Matlab files and commented the code

--- Simscape_CAD_Raibert_init.m
+++ Simscape_CAD_Raibert_init.m
@@ -9,9 +9,9 @@ hip_motor_path = "Simscape_CAD_Raibert/Hopping Robot Model/Hip Motor AK80-6";
 hip_plate_path = "Simscape_CAD_Raibert/Hopping Robot Model/Hip Plate";
 
 set_param(shank_path,"ExtGeomFileName",strcat(filepath,"shank.STEP"));
-set_param(thigh_path,"ExtGeomFileName",strcat(filepath,"thigh.STEP"))
-set_param(pulley_path,"ExtGeomFileName",strcat(filepath,"pulley.STEP"))
-set_param(knee_motor_path,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"))
-set_param(knee_plate_path,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"))
-set_param(hip_motor_path,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"))
-set_param(hip_plate_path,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"))
+set_param(thigh_path,"ExtGeomFileName",strcat(filepath,"thigh.STEP"));
+set_param(pulley_path,"ExtGeomFileName",strcat(filepath,"pulley.STEP"));
+set_param(knee_motor_path,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"));
+set_param(knee_plate_path,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"));
+set_param(hip_motor_path,"ExtGeomFileName",strcat(filepath,"AK80-6 Motor.STEP"));
+set_param(hip_plate_path,"ExtGeomFileName",strcat(filepath,"hip motor plate.STEP"));
